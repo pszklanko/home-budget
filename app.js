ec64957ea2361eb08b732a8672649211329020ef
pszklanko <pszklanko@gmail.com>
2017-08-08 23:00:00 +0200

    basic router added

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+const home = { template: '<div>HOME</div>' }
+const needs = { template: '<div>NEEDS</div>' }
+
+const routes = [
+    { path: '/home', component: home },
+    { path: '/needs', component: needs }
+]
+
+const router = new VueRouter({
+    routes
+})
+
 Vue.component('todo-item', {
     props: ['todo'],
     template: '<div>{{todo}}</div>'
 })
 
 new Vue({
-    el: '.app',
+    router,
     data: {
         message: 'SIEMA!'
     }
-})
+}).$mount("#app")
